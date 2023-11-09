3f299ec95fc4ced93453e4b4a3d501b8375e5b09
elinjakobsen <126721502+elinkrull@users.noreply.github.com>
2023-11-09 21:48:06 +0100

    added all html files and addded code to html and css files

---
+++ script.js/contact.js
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<link rel="stylesheet" href="styles.css" />
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Contact</title>
+	<script src="https://kit.fontawesome.com/727f4fec37.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+	<header>
+		<nav>
+			<a href="index.html">Home</a>
+			<a href="about.html" class="onpage">About</a>
+			<a href="blogs.html">Plants</a>
+			<a href="contact.html">Contact</a>
+		</nav>
+	</header>
+
+	<main>
+		<div>
+			Hello, fellow plant enthusiasts! I'm Elin, and I'm thrilled to welcome you to my green oasis in the
+			digital world. At 37 years young, I've discovered that there's no age limit on falling head over heels for
+			the wonderful world of indoor plants.
+		</div>
+		<div>
+			My journey into the world of greenery began like many others — a small potted plant gifted to me by a
+			friend.
+			Little did I know that this simple act of kindness would set me on a path of fascination and growth.
+		</div>
+		<div>
+			What started as a single plant quickly turned into an ever-growing collection, each with its own personality
+			and story. As I nurtured my leafy companions, I realized that they were not just decorations but living,
+			breathing beings that brought life and vibrancy to my home.
+		</div>
+		<div>
+			'Plantastic Living' is more than just a blog; it's a labor of love. It's my way of sharing my passion for
+			plants, their remarkable qualities, and the joy they bring to everyday life. From transforming my living
+			space into a lush paradise to exploring the countless species and varieties that grace our homes, I'm here
+			to document my journey and share the insights I've gathered along the way.
+		</div>
+		<div>
+			So, whether you're a seasoned plant parent or just starting your green journey, I invite you to join me in
+			this exciting adventure of 'Plantastic Living.' Let's connect, grow, and explore the beauty of the botanical
+			world together.
+		</div>
+	</main>
+
+	<footer>
+		<div>
+			<p>By Elin Thoen Jakobsen</p>
+			<i class="fa-brands fa-facebook"></i>
+			<i class="fa-brands fa-instagram"></i>
+			<i class="fa-brands fa-snapchat"></i>
+			<i class="fa-brands fa-tiktok"></i>
+			<i class="fa-brands fa-youtube"></i>
+		</div>
+	</footer>
+</body>
+
+</html>
