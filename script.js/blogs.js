79806b3b55630bb2a48a2f835e4e2d902bf19f45
elinjakobsen <126721502+elinkrull@users.noreply.github.com>
2023-11-16 00:45:56 +0100

    added code to blogs.js, blog-posts are displayedin blogs.html. added code to stylesheet

--- script.js/blogs.js
+++ script.js/blogs.js
@@ -1,46 +1,87 @@
+//Error handling
+
+function showError(message) {
+	const errorContainer = document.getElementById("blog-posts");
+	errorContainer.innerHTML += `<h2>${message}</h2>`;
+  }
+
 // Loading indicator
 
 function showLoadingIndicator() {
-	const loadingIndicator = document.getElementById("blog-posts")
-	showLoadingIndicator.innerHTML = "<li>Loading...</li>";
+	const loadingIndicator = document.getElementById("blog-posts");
+	loadingIndicator.innerHTML = "<li>Loading...</li>";
 }
 
-// API call 
+// API call blog posts
 
 const url = "https://projectexam1.elinjakobsen.no/wp-json/wp/v2/posts/";
 
-async function getPosts {
+async function getPosts() {
 	try {
 		showLoadingIndicator();
 		const response = await fetch(url);
 	  	const result = await response.json();
-	  	return result;  
-	} 
-	
-	// catch (error) {
-	//   throw new Error("Sorry, we could not fetch the blog-posts");
-	// }
+		return(result);
+
+	} catch (error) {
+		throw new Error("Sorry, we could not fetch the blog posts");
+	  }
 }
 
-async function displayPosts {
+async function displayPosts() {	
 	try {
 		const blogPosts = await getPosts();
 		const blogPostsContainer = document.getElementById("blog-posts");
 		blogPostsContainer.innerHTML = "";
 
-		for (let i = 0; i < 10, i++) {
+		for (let i = 0; i < 10; i++) {
 			const post = blogPosts[i];
 
-			blogPostsContainer.innerHTML += `<div class= "all-posts-container">
-			<a href="specificblog.html?id=${post.id}&title=${post.title.rendered}</a>
-			<h2>${post.title.rendered}</h2>
-			</div>`
+			blogPostsContainer.innerHTML += `<div class="all-posts-container">
+			<a href="specificblog.html?id=${post.id}&title=${post.title}"><h2>${post.title.rendered}</h2></a>
+			</div>`;
 		}
-	}
+	} catch (error) {
+		showError(error.message);
+	  }
+}
 	
-	// catch (error) {
-	// 	showError(error.message);
-	//   }
+displayPosts();
+
+// API call to get media
+
+
+const mediaURL = "https://projectexam1.elinjakobsen.no/wp-json/wp/v2/media";
+
+
+async function getPictures() {
+	try {
+		showLoadingIndicator();
+		const response = await fetch(url);
+	  	const result = await response.json();
+		return(result);
+
+	} catch (error) {
+		throw new Error("Sorry, we could not fetch the blog posts");
+	  }
 }
 
-displayPosts(); 
+async function displayPictures() {
+	try {
+		const blogPictures = await getPictures();
+		const blogPicturesContainer = document.getElementById("blog-pictures");
+		blogPicturesContainer.innerHTML = "";
+	
+		for (let i = 0; i < blogPictures.length; i++) {
+			const picture = blogPictures[i];
+
+			blogPicturesContainer.innerHTML += `<div class="all-pictures-container">
+			<a href="specificblog.html?id=${picture.id}&title=${picture.title}"><img src="${picture.media_details.file}" alt="${picture.title.rendered}" class="pictures"></a>
+			<h2>${post.title.rendered}</h2>
+			</div>`;
+		}
+	} catch (error) {
+		showError(error.message);
+	  }
+
+}
