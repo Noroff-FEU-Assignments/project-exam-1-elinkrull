28dbf431a3590d2f7a7aa777588cacadf799ec1f
elinjakobsen <126721502+elinkrull@users.noreply.github.com>
2023-11-16 14:47:01 +0100

    added code to specificblog.js

--- script.js/blogs.js
+++ script.js/blogs.js
@@ -38,7 +38,7 @@ async function displayPosts() {
 			const post = blogPosts[i];
 
 			blogPostsContainer.innerHTML += `<div class="all-posts-container">
-			<a href="specificblog.html?id=${post.id}&title=${post.title}"><h2>${post.title.rendered}</h2></a>
+			<a href="specificblog.html?id=${post.id}&title=${post.title.rendered}"><h2>${post.title.rendered}</h2></a>
 			</div>`;
 		}
 	} catch (error) {
@@ -51,37 +51,37 @@ displayPosts();
 // API call to get media
 
 
-const mediaURL = "https://projectexam1.elinjakobsen.no/wp-json/wp/v2/media";
+// const mediaURL = "https://projectexam1.elinjakobsen.no/wp-json/wp/v2/media";
 
 
-async function getPictures() {
-	try {
-		showLoadingIndicator();
-		const response = await fetch(url);
-	  	const result = await response.json();
-		return(result);
+// async function getPictures() {
+// 	try {
+// 		showLoadingIndicator();
+// 		const response = await fetch(url);
+// 	  	const result = await response.json();
+// 		return(result);
 
-	} catch (error) {
-		throw new Error("Sorry, we could not fetch the blog posts");
-	  }
-}
+// 	} catch (error) {
+// 		throw new Error("Sorry, we could not fetch the blog posts");
+// 	  }
+// }
 
-async function displayPictures() {
-	try {
-		const blogPictures = await getPictures();
-		const blogPicturesContainer = document.getElementById("blog-pictures");
-		blogPicturesContainer.innerHTML = "";
+// async function displayPictures() {
+// 	try {
+// 		const blogPictures = await getPictures();
+// 		const blogPicturesContainer = document.getElementById("blog-pictures");
+// 		blogPicturesContainer.innerHTML = "";
 	
-		for (let i = 0; i < blogPictures.length; i++) {
-			const picture = blogPictures[i];
+// 		for (let i = 0; i < blogPictures.length; i++) {
+// 			const picture = blogPictures[i];
 
-			blogPicturesContainer.innerHTML += `<div class="all-pictures-container">
-			<a href="specificblog.html?id=${picture.id}&title=${picture.title}"><img src="${picture.media_details.file}" alt="${picture.title.rendered}" class="pictures"></a>
-			<h2>${post.title.rendered}</h2>
-			</div>`;
-		}
-	} catch (error) {
-		showError(error.message);
-	  }
+// 			blogPicturesContainer.innerHTML += `<div class="all-pictures-container">
+// 			<a href="specificblog.html?id=${picture.id}&title=${picture.title}"><img src="${picture.media_details.file}" alt="${picture.title.rendered}" class="pictures"></a>
+// 			<h2>${post.title.rendered}</h2>
+// 			</div>`;
+// 		}
+// 	} catch (error) {
+// 		showError(error.message);
+// 	  }
 
-}
+// }
---
+++ script.js/specificblog.js
@@ -0,0 +1,67 @@
+//Error handling
+function showError(message) {
+	const errorContainer = document.getElementById("specific-post-container");
+	errorContainer.innerHTML += `<h2>${message}</h2>`;
+  }
+  
+  //Loading indicator
+  function showLoadingIndicator() {
+	const loadingIndicator = document.getElementById("specific-post-container");
+	loadingIndicator.innerHTML = "<li>Loading...</li>";
+  }
+  
+  // Function to extract the blog post ID from the query parameter
+  function getPostIdFromQuery() {
+	try {
+	  const urlParams = new URLSearchParams(window.location.search);
+	  const id = urlParams.get("id");
+	  return id;
+	} catch (error) {
+	  throw new Error("Sorry, we could not fetch the Id");
+	}
+  }
+  
+  // Function to extract the blog post title from the query parameter
+  function getPostTitleFromQuery() {
+	try {
+	  const urlParams = new URLSearchParams(window.location.search);
+	  const title = urlParams.get("title");
+	  return title;
+	} catch (error) {
+	  throw new Error("Sorry, we could not fetch the title");
+	}
+  }
+  
+  // Function to fetch blog post details using the blog post ID and populate the details section
+  async function fetchPostDetail() {
+	try {
+	  showLoadingIndicator();
+	  const postId = getPostIdFromQuery();
+  
+	  const title = getPostTitleFromQuery();
+  
+	  if (!postId) showError(message);
+
+  	  const response = await fetch(`https://projectexam1.elinjakobsen.no/wp-json/wp/v2/posts/${postId}`);
+	  const postDetail = await response.json();
+	  console.log(postDetail);
+  
+	  const postTitleContainer = document.getElementById("title");
+	  const postDetailContainer = document.getElementById("specific-post-container");
+	  postDetailContainer.innerHTML = "";
+  
+	  //Add blog post title to the title of the page
+	//   postTitleContainer.textContent = title;
+	  postDetailContainer.innerHTML += `           
+		<div class="specific-blog-post">
+			<h1>${postDetail.title.rendered}</h1> 
+			<p>${postDetail.content.rendered}</p>
+		</div>
+		`;
+	} catch (error) {
+		console.log(error.message);
+	//   showError(error.message);
+	}
+  }
+  
+  fetchPostDetail();
