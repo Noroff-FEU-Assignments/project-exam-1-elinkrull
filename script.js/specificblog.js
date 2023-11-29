02e71a30e13efb8367aebd08bdade3c0f22a77e3
elinjakobsen <126721502+elinkrull@users.noreply.github.com>
2023-11-29 15:42:51 +0100

    added code to specificblog.html, modal in progress

--- script.js/specificblog.js
+++ script.js/specificblog.js
@@ -17,21 +17,24 @@ function getPostIdFromQuery() {
 	const id = urlParams.get("id");
 	console.log(id);
 	return id;
+
 	} catch (error) {
 		throw new Error("Sorry, we could not fetch the Id");
 	}
 }
 
+
 //  Function to extract the blog post title from the query parameter
-function getPostTitleFromQuery() {
-	try {
-		const urlParams = new URLSearchParams(window.location.search);
-		const title = urlParams.get("title.rendered");
-		return title;
-	}	catch (error) {
-		throw new Error("Sory, we could not fetch the Title");
-	}
-}
+// function getPostTitleFromQuery() {
+// 	try {
+// 		const urlParams = new URLSearchParams(window.location.search);
+// 		const title = urlParams.get("title");
+// 		console.log(title);
+// 		return title;
+// 	}	catch (error) {
+// 		throw new Error("Sorry, we could not fetch the Title");
+// 	}
+// }
 
 async function fetchPostDetail() {
 	try {
@@ -39,9 +42,12 @@ async function fetchPostDetail() {
 	
 	const postId = getPostIdFromQuery();
 	
-	const title = getPostTitleFromQuery();
+	// const title = getPostTitleFromQuery();
 
-	if(!postId) showError("The blog posts ID is not avaliable");
+	if(!postId) {
+		showError("The blog post ID is not avaliable");
+		return;
+	}
 
 	const response = await fetch(`https://projectexam1.elinjakobsen.no/wp-json/wp/v2/posts/${postId}`);
 	if (!response.ok) {
@@ -49,21 +55,58 @@ async function fetchPostDetail() {
 	 }
 	const postDetail = await response.json();
 
-	const postTitleContainer = document.getElementById("title");
+	// const postTitleContainer = document.getElementById("title");
 	const postDetailContainer = document.getElementById("specific-post-container");
 	postDetailContainer.innerHTML= "";
 
-
 	// Add post title to the title of the page
-	postTitleContainer.textContent = title;
+	// postTitleContainer.textContent = title;
 	postDetailContainer.innerHTML = `
 	<h2>${postDetail.id}: ${postDetail.title.rendered}</h2>
 	<p>${postDetail.content.rendered}</p>
+
+		<button data-open-modal></button>
+
+		<dialog data-modal class="modal">
+			<div>This is a modal</div>
+			<button data-close-modal>Close</button>
+		</dialog>
 	`;
-} 
-catch (error) {
+
+const openButton = document.querySelector("[data-open-modal]")
+const closeButton = document.querySelector("[data-close-modal]")
+const modal = document.querySelector("[data-modal]")
+
+openButton.addEventListener("click", () => {
+	modal.showModal()
+})
+
+closeButton.addEventListener("click", () => {
+	modal.close()
+})
+
+modal.addEventListener("click", e => {
+const dialogDimensions = modal.getBoundingClientRect()
+if (
+	e.clientX < dialogDimensions.left ||
+	e.clientX < dialogDimensions.right ||
+	e.clientY < dialogDimensions.top||
+	e.clientY < dialogDimensions.bottom
+) {
+	modal.close()
+}
+})
+
+
+} catch (error) {
 	showError(error.message);
   }
 }
 
 fetchPostDetail();
+
+// 	if(!name) {
+// 		showError("The blog posts title is not available");
+// 		return;
+// 	}
+
