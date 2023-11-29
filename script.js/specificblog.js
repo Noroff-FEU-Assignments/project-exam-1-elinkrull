6e6e4767df0991bdac0cd50e958c7561c3e2e3b0
elinjakobsen <126721502+elinkrull@users.noreply.github.com>
2023-11-29 08:23:57 +0100

    working on blog post title on specificblog.js - getting errors

--- script.js/specificblog.js
+++ script.js/specificblog.js
@@ -12,20 +12,26 @@ function showLoadingIndicator() {
 
 // Function to extract the blog post ID from the query parameter
 function getPostIdFromQuery() {
+	try {
 	const urlParams = new URLSearchParams(window.location.search);
-	return urlParams.get("id");
+	const id = urlParams.get("id");
+	console.log(id);
+	return id;
+	} catch (error) {
+		throw new Error("Sorry, we could not fetch the Id");
+	}
 }
 
- // Function to extract the blog post title from the query parameter
-// function getPostTitleFromQuery() {
-// 	try {
-// 		const urlParams = new URLSearchParams(window.location.search);
-// 		const title = urlParams.get("title");
-// 		return title;
-// 	}	catch (error) {
-// 		throw new Error("Sory, we could not fetch the Title");
-// 	}
-// }
+//  Function to extract the blog post title from the query parameter
+function getPostTitleFromQuery() {
+	try {
+		const urlParams = new URLSearchParams(window.location.search);
+		const title = urlParams.get("title.rendered");
+		return title;
+	}	catch (error) {
+		throw new Error("Sory, we could not fetch the Title");
+	}
+}
 
 async function fetchPostDetail() {
 	try {
@@ -33,25 +39,29 @@ async function fetchPostDetail() {
 	
 	const postId = getPostIdFromQuery();
 	
-	// const title = getPostTitleFromQuery();
+	const title = getPostTitleFromQuery();
 
-	if(!postId) showError(message);
+	if(!postId) showError("The blog posts ID is not avaliable");
 
 	const response = await fetch(`https://projectexam1.elinjakobsen.no/wp-json/wp/v2/posts/${postId}`);
+	if (!response.ok) {
+		throw new Error("HTTP error");
+	 }
 	const postDetail = await response.json();
 
-	// const postTitleContainer = document.getElementById("title");
+	const postTitleContainer = document.getElementById("title");
 	const postDetailContainer = document.getElementById("specific-post-container");
 	postDetailContainer.innerHTML= "";
 
 
-	//Add post title to the title of the page
-	// postTitleContainer.textContent = title;
+	// Add post title to the title of the page
+	postTitleContainer.textContent = title;
 	postDetailContainer.innerHTML = `
 	<h2>${postDetail.id}: ${postDetail.title.rendered}</h2>
 	<p>${postDetail.content.rendered}</p>
 	`;
-} catch (error) {
+} 
+catch (error) {
 	showError(error.message);
   }
 }
